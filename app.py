21197250f85113a8520e8e39d0584e2c31466bcb
bln121 <77780135+bln121@users.noreply.github.com>
2023-05-18 09:33:25 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import urllib.request
+
+def main():
+    st.title("Display Image in Streamlit DataFrame")
+    
+    # Create a DataFrame
+    df = pd.DataFrame({
+        'Image': ['https://raw.githubusercontent.com/<USERNAME>/<REPO_NAME>/<BRANCH_NAME>/<PATH_TO_IMAGE>'],
+        'Caption': ['Image Caption']
+    })
+    
+    # Retrieve the image from the GitHub repository
+    df['Image'] = df['Image'].apply(lambda url: Image.open(urllib.request.urlopen(url)))
+    
+    # Display the DataFrame with the image
+    st.dataframe(df)
+
+if __name__ == '__main__':
+    main()
