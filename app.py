66d115f6c638a133ba3997104be7b185a6e911a4
bln121 <77780135+bln121@users.noreply.github.com>
2023-05-19 09:23:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
-import pandas as pd
-from PIL import Image
-import requests
-from io import BytesIO
 import streamlit as st
+from PIL import Image
 
-# Fetch the image from Google
-image_url = "https://tse4.mm.bing.net/th?id=OIP.B6TRJxR2eA7DYGmNyaYG3AHaJX&pid=Api&P=0&h=180"
-response = requests.get(image_url)
-image = Image.open(BytesIO(response.content))
+image = Image.open('https://tse4.mm.bing.net/th?id=OIP.B6TRJxR2eA7DYGmNyaYG3AHaJX&pid=Api&P=0&h=180')
 
-
-# Create the dataframe
-data = {'Image': [image]}
-df = pd.DataFrame(data)
-
-st.write(df)
+st.image(image, caption='Sunrise by the mountains')
