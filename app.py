9494b951879b4bdd58f2f483db136ca667869608
bln121 <77780135+bln121@users.noreply.github.com>
2023-05-19 09:30:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+import pandas as pd
 import streamlit as st
-from PIL import Image
 
-image = Image.open('https://tse4.mm.bing.net/th?id=OIP.B6TRJxR2eA7DYGmNyaYG3AHaJX&pid=Api&P=0&h=180')
+# Create a DataFrame
+data = {
+    'Name': ['John', 'Jane', 'Adam'],
+    'Age': [25, 30, 35],
+    'Image': ['https://tse4.mm.bing.net/th?id=OIP.B6TRJxR2eA7DYGmNyaYG3AHaJX&pid=Api&P=0&h=180', '', '']
+}
+df = pd.DataFrame(data)
 
-st.image(image, caption='Sunrise by the mountains')
+# Iterate over the DataFrame and display the images
+for index, row in df.iterrows():
+    image_path = row['Image']
+    st.write(f"Name: {row['Name']}")
+    st.write(f"Age: {row['Age']}")
+    st.image(image_path, caption=row['Name'], use_column_width=True)
