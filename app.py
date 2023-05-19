58e565e269a9cf7986f7388ce33538bf0605cd7f
bln121 <77780135+bln121@users.noreply.github.com>
2023-05-19 09:17:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 from PIL import Image
 import requests
 from io import BytesIO
+import streamlit as st
 
 # Fetch the image from Google
 image_url = "https://tse4.mm.bing.net/th?id=OIP.B6TRJxR2eA7DYGmNyaYG3AHaJX&pid=Api&P=0&h=180"
