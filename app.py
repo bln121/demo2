29caa739fac772bf1cfc6910b75535b9e6af4de6
bln121 <77780135+bln121@users.noreply.github.com>
2023-06-13 10:22:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 import streamlit as st
 import pandas as pd
-import gspread
 
-# Open the Google Sheets document
-gc = gspread.service_account()
-sheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/1jJH8_IAVNIJdNsHAdDLy3DaebK8nb_Vy9ca5I7A2CfY/edit?usp=sharing')
+# URL of the Google Sheets document
+sheet_url = 'https://docs.google.com/spreadsheets/d/1jJH8_IAVNIJdNsHAdDLy3DaebK8nb_Vy9ca5I7A2CfY/edit?usp=sharing'
 
-# Get the first sheet of the document
-worksheet = sheet.get_worksheet(0)
+# Read the Google Sheets document into a Pandas DataFrame
+df_list = pd.read_html(sheet_url)
 
-# Get all values from the worksheet
-data = worksheet.get_all_values()
-
-# Convert the data to a Pandas DataFrame
-df = pd.DataFrame(data)
+# Select the appropriate DataFrame from the list
+df = df_list[0]  # Change the index if needed
 
 # Display the DataFrame in Streamlit
 st.write(df)
