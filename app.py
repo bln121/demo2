27c59512ece0a0358499fbc65fe5ad87a52ffc46
bln121 <77780135+bln121@users.noreply.github.com>
2023-06-13 09:17:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import pandas as pd
 import gspread
 
 # Open the Google Sheets document
-sheet = gspread.open_by_url('https://docs.google.com/spreadsheets/d/1jJH8_IAVNIJdNsHAdDLy3DaebK8nb_Vy9ca5I7A2CfY/edit?usp=sharing')
+gc = gspread.service_account()
+sheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/1jJH8_IAVNIJdNsHAdDLy3DaebK8nb_Vy9ca5I7A2CfY/edit?usp=sharing')
 
 # Get the first sheet of the document
 worksheet = sheet.get_worksheet(0)
